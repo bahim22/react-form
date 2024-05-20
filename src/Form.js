acfef1e60742cfb55b95873c9d0ad5ff5c2fc5c6
Hima Balde <78245175+bahim22@users.noreply.github.com>
2024-05-20 03:25:47 -0400

    Update Form.js

--- src/Form.js
+++ src/Form.js
@@ -1,9 +1,14 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 // import './styles/Form.css';
-import './styles/Info.css';
+// import './styles/Info.css';
 import ScopedCssBaseline from '@mui/material/ScopedCssBaseline';
 import Button from '@mui/material/Button';
+import { Box, TextField } from '@mui/material';
+import FormLabel from '@mui/material/FormLabel';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+
 // require('dotenv').config();
 
 export default function FormPro() {
@@ -73,68 +78,64 @@ export default function FormPro() {
         </button>;
     }; */
     return (
-        <ScopedCssBaseline>
-            <header id='contact'>
-                <div className='sm:min-w-full lg:min-w-full bg-gray-400 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-center justify-around shadow-md shadow-teal-400'>
-                    {/* <img width='100%' height='100' src='https://i.imgur.com/qkdpN.png' alt='logo' />
-                    <img title='my puppy' width='50%' height='50' src='/pup.jpeg' alt='logo' />
-                    */}
-                    {/* <img
-                        src='https://lists.office.com/Images/99e4f947-156c-4d29-a851-b2dc4d0f579e/467d1d78-fabd-40b9-992c-61e4a32208b2/T5FH5JPCKJU66BWFKVA8LK3FFQ/6fa7a51b-2dd0-4786-9687-4eef36143313'
-                        alt='365 Form'
-                        title='365 Form'
-                        loading='lazy'
-                        width='100%'
-                        height='100'
-                    /> */}
-                    {/* <img src='/pup.jpeg' alt='logo card' /> */}
-                </div>
-            </header>
-            <br />
-            <div className='sm:min-w-full lg:min-w-full bg-gray-400 rounded-lg overflow-hidden sm:mr-10 p-10 flex-wrap items-center justify-around shadow-md shadow-teal-400'>
-
-                <h1 className='title-font sm:text-2xl text-3xl mb-4 font-medium text-blue-400 items-center text-center'>
-                    Contact Me Today
-                </h1>
-                <form
-                    onSubmit={handleOnSubmit}
-                    method='POST'
-                    className='sm:min-w-full lg:w-full bg-gray-400 rounded-lg overflow-hidden sm:mr-10 p-10 flex-wrap items-center justify-around shadow-md shadow-teal-400'>
-                    <label htmlFor='email'>Email</label>
-                    <input
+        <>
+            <Box id='email' sx={{ display: 'flex', flexDirection: 'column', gap: 2 }} align={'center'}>
+                <Typography variant='h4' align='center' component='h2' sx={{ mb: 2 }}>
+                    Contact Today to Sign Up!
+                </Typography>
+                <form onSubmit={handleOnSubmit} method='POST'>
+                    <FormLabel htmlFor='email'></FormLabel>
+                    {/* <label htmlFor='email'>Email</label> */}
+                    <TextField
                         id='email'
+                        fullWidth
                         type='email'
+                        placeholder='Enter your email.'
                         name='_replyto'
+                        aria-colspan={80}
+                        rows={8}
                         onChange={handleOnChange}
                         required
+                        // variant='Filled'
                         value={inputs.email}
                     />
-                    <label htmlFor='message'>Message</label>
-                    <textarea id='message' name='message' onChange={handleOnChange} required value={inputs.message} />
-                    <br />
-                    <Button variant='contained' type='submit' disabled={status.submitting}>
+                    <Divider variant='middle' textAlign='center' sx={{ color: 'primary' }} />
+                    <FormLabel htmlFor='message'></FormLabel>
+                    {/* <label htmlFor='message'>Message</label> */}
+                    <TextField
+                        id='message'
+                        fullWidth
+                        name='message'
+                        placeholder='Enter your message.'
+                        onChange={handleOnChange}
+                        required
+                        value={inputs.message}
+                    />
+                    <Divider variant='middle' textAlign='center' sx={{ color: 'primary' }} />
+                    <Button variant='contained' type='submit' fullWidth disabled={status.submitting}>
                         {!status.submitting ? (!status.submitted ? 'Submit' : 'Submitted') : 'Submitting...'}
                     </Button>
                     {/* <button type='submit' disabled={status.submitting}>
                         {!status.submitting ? (!status.submitted ? 'Submit' : 'Submitted') : 'Submitting...'}
                     </button> */}
                 </form>
-                <div className='lg:min-w-fit lg:w-fit md:w-1/2 flex'>
+                {/* <div>
                     <img
                         className='object-cover object-center rounded'
-                        alt='hima b'
-                        title='hima b'
+                        alt='ISV'
+                        title='ISV'
                         loading='eager'
-                        src='/hima2.jpeg'
-                        width='50%'
-                        height='300'
+                        src='./assets/logo640.png'
+                        // src='/logo640.png'
+                        width='auto'
+                        height='auto'
                     />
-                <img title='my puppy' width='50%' height='50' src='/pup.jpeg' alt='logo' />
-                </div>
-            </div>
+                    <img title='my puppy' width='50%' height='50' src='/pup.jpeg' alt='logo' />
+                </div>*/}
+            </Box>
             {status.info.error && <div className='error'>Error: {status.info.msg}</div>}
             {!status.info.error && status.info.msg && <p>{status.info.msg}</p>}
-        </ScopedCssBaseline>
+        </>
     );
 }
 
