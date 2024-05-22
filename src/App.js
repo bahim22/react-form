9d4c60a51850e1c5efcdbff23f467f99a24bac24
Hima Balde <78245175+bahim22@users.noreply.github.com>
2024-05-21 22:14:35 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Projects from './components/Projects';
 //import MenuBar from './components/Menu';
 import FormPro from './Form';
 import Copyright from './components/CopyrightFoot';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 // import Skills2  from './components/Skills';
 // import Software2  from './components/Software';
 // import { Software } from './components/Software';
@@ -14,16 +16,34 @@ import Copyright from './components/CopyrightFoot';
 // import CardMain from './components/CardMain';
 import AppAppBar from './components/Menu24';
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#757ce8',
+      main: '#3f50b5',
+      dark: '#002884',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#ff7961',
+      main: '#f44336',
+      dark: '#ba000d',
+      contrastText: '#000',
+    },
+  },
+});
+
 function App() {
     return (
-        <>
-            <AppAppBar />
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <AppAppBar mode='dark' />
             <About />
             <Projects />
             <Contact />
             <FormPro />
             <Copyright />
-        </>
+        </ThemeProvider>
     );
 }
 
